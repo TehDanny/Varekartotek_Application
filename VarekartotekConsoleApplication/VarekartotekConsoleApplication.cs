79d45829215e85e4a66ef7f2a0c067512eab0f69
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-06-20 01:06:06 +0200

    Opgave g, h og i

--- VarekartotekConsoleApplication/VarekartotekConsoleApplication.cs
+++ VarekartotekConsoleApplication/VarekartotekConsoleApplication.cs
@@ -25,11 +25,11 @@ namespace VarekartotekApplication
 		// metode der opretter test varer
 		public void OpretTestVarer ()
 		{
-			varesamling.Add( new Vare (1001, "A4 blok ternet med huller", 10, 3.98f) );
-			varesamling.Add( new Vare (1002, "A4 blok linieret med huller", 20, 3.88f) );
-			varesamling.Add( new Vare (1003, "Blyant Viking 400x2", 110, 0.64f) );
-            varesamling.Add(new Vare(1004, "Lenovo bærbar", 2, 4999f));
-            varesamling.Add(new Vare(1005, "Blyantspidser", 57, 95f));
+			varesamling.Add( new Vare (1001, "A4 blok ternet med huller", "Blok", 10, 3.98f) );
+			varesamling.Add( new Vare (1002, "A4 blok linieret med huller", "Blok", 20, 3.88f) );
+			varesamling.Add( new Vare (1003, "Blyant Viking 400x2", "Æske", 110, 0.64f) );
+            varesamling.Add(new Vare(1004, "Lenovo bærbar", "Styk", 2, 3999f));
+            varesamling.Add(new Vare(1005, "Blyantspidser", "Styk", 57, 9.5f));
         }
 
 		// metode der udskriver en oversigt over alle varer i samlingen
@@ -48,9 +48,10 @@ namespace VarekartotekApplication
 			Console.WriteLine("--------------------------------------------------");
 			Console.WriteLine("Varenr: " + vare.Varenr);
 			Console.WriteLine("Betegnelse: " + vare.Betegnelse);
+            Console.WriteLine("Enhed: " + vare.Enhed);
 			Console.WriteLine("Antal enheder på lager: " + vare.AntalPåLager);
-            Console.WriteLine("Salgspris pr. enhed: " + vare.SalgsEnhedsPris + " kr.");
-			Console.WriteLine("Salgsværdi: " + vare.Salgsværdi() + " kr.");//NB: metodekald
+            Console.WriteLine("Salgspris pr. enhed: " + vare.SalgsEnhedsPris.ToString("c2"));
+			Console.WriteLine("Salgsværdi: " + vare.Salgsværdi().ToString("c2") );//NB: metodekald
 		}
 	}
 
@@ -60,14 +61,16 @@ namespace VarekartotekApplication
 		// vare-objektes memory (datafelter)
 		public int		Varenr;
 		public string	Betegnelse;
-		public int		AntalPåLager;
+        public string   Enhed;
+        public int		AntalPåLager;
 		public float	SalgsEnhedsPris;			// aktuel excl. moms
 
 		// constructor til brug for skabelse af nyt vare-object
-		public Vare (int varenr, string betegnelse,	int antalPåLager, float salgsEnhedsPris)
+		public Vare (int varenr, string betegnelse, string enhed, int antalPåLager, float salgsEnhedsPris)
 		{
 			this.Varenr = varenr;
 			this.Betegnelse = betegnelse;
+            this.Enhed = enhed;
 			this.AntalPåLager = antalPåLager;
 			this.SalgsEnhedsPris = salgsEnhedsPris;
 		}
@@ -75,7 +78,7 @@ namespace VarekartotekApplication
 		// metode for beregning af den salgsværdien det samlede antal
 		public double Salgsværdi ()
 		{
-			double salgsværdi = this.SalgsEnhedsPris * AntalPåLager;
+			double salgsværdi = this.SalgsEnhedsPris * 1.25;
 			return salgsværdi;
 		}
 	}
