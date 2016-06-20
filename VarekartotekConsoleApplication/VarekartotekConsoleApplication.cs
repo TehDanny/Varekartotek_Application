3260f6ad012a6c5486aeb199fd5f372b1437919f
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2016-06-20 12:19:53 +0200

    Opgave m

--- VarekartotekConsoleApplication/VarekartotekConsoleApplication.cs
+++ VarekartotekConsoleApplication/VarekartotekConsoleApplication.cs
@@ -28,7 +28,7 @@ namespace VarekartotekApplication
 			varesamling.Add( new Vare (1001, "A4 blok ternet med huller", "Blok", 10, 3.98f, 2.56m) );
 			varesamling.Add( new Vare (1002, "A4 blok linieret med huller", "Blok", 20, 3.88f, 2.73m) );
 			varesamling.Add( new Vare (1003, "Blyant Viking 400x2", "Æske", 110, 0.64f, 0.45m) );
-            varesamling.Add(new Vare(1004, "Lenovo bærbar", "Styk", 2, 3999f, 1500m));
+            varesamling.Add(new Vare(1004, "Lenovo bærbar", "Styk", 2, 4999f, 1500m));
             varesamling.Add(new Vare(1005, "Blyantspidser", "Styk", 57, 9.5f, 0.53m));
         }
 
@@ -53,7 +53,8 @@ namespace VarekartotekApplication
             Console.WriteLine("Salgspris pr. enhed: " + vare.SalgsEnhedsPris.ToString("c2"));
 			Console.WriteLine("Salgsværdi: " + vare.Salgsværdi().ToString("c2") );//NB: metodekald
             Console.WriteLine("Indkøbspris pr. enhed: " + vare.IndkøbEnhedsPris.ToString("c2"));
-		}
+            Console.WriteLine("Indkøbsværdi: " + vare.Indkøbsværdi().ToString("c2"));
+        }
 	}
 
 	// definition af Vare (data og metoder der findes for hver enkelt vare-object
@@ -78,11 +79,18 @@ namespace VarekartotekApplication
             this.IndkøbEnhedsPris = indkøbsEnhedsPris;
 		}
 
-		// metode for beregning af den salgsværdien det samlede antal
+		// metode for beregning af salgsværdien for det samlede antal
 		public double Salgsværdi ()
 		{
-			double salgsværdi = this.SalgsEnhedsPris * 1.25;
+			double salgsværdi = this.SalgsEnhedsPris * this.AntalPåLager;
 			return salgsværdi;
 		}
-	}
+
+        // metode for beregning af indkøbsværdien for det samlede antal
+        public decimal Indkøbsværdi()
+        {
+            decimal indkøbsværdi = this.IndkøbEnhedsPris * this.AntalPåLager;
+            return indkøbsværdi;
+        }
+    }
 }
